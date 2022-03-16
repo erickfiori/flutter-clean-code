9af30cea0a4a3efd3ccfbf8f8e7c7257740862f1
erickfiori <e.felipe.santos@hotmail.com>
2022-03-16 16:50:19 -0300

    Handle Response Method

--- fordev/lib/infra/http_adapter.dart
+++ fordev/lib/infra/http_adapter.dart
@@ -16,21 +16,25 @@ class HttpAdapter implements HttpClient {
 
     final response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
 
-    switch (response.statusCode) {
-      case 200:
-        return response.body.isNotEmpty ? jsonDecode(response.body) : null;
-      case 204:
-        return null;
-      case 400:
-        throw HttpError.badRequest;
-      case 401:
-        throw HttpError.unauthorized;
-      case 403:
-        throw HttpError.forbidden;
-      case 404:
-        throw HttpError.notFound;
-      default:
-        throw HttpError.serverError;
-    }
+    return _handleResponse(response);
+  }
+}
+
+_handleResponse(Response response) {
+  switch (response.statusCode) {
+    case 200:
+      return response.body.isNotEmpty ? jsonDecode(response.body) : null;
+    case 204:
+      return null;
+    case 400:
+      throw HttpError.badRequest;
+    case 401:
+      throw HttpError.unauthorized;
+    case 403:
+      throw HttpError.forbidden;
+    case 404:
+      throw HttpError.notFound;
+    default:
+      throw HttpError.serverError;
   }
 }
