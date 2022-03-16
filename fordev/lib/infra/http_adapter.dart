1375c87a81fb05a95d2fbad0b633b1b4728dceef
erickfiori <e.felipe.santos@hotmail.com>
2022-03-16 16:14:04 -0300

    Ensure HttpAdapter returns data if Post returns statusCode 200

---
+++ fordev/lib/infra/http_adapter.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:fordev/data/http/http.dart';
+import 'package:http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  @override
+  Future<Map> request({@required String url, @required String method, Map body}) async {
+    final headers = {'content-type': 'application/json', 'accept': 'application/json'};
+    final jsonBody = body != null ? jsonEncode(body) : null;
+
+    final response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+    return jsonDecode(response.body);
+  }
+}
--- fordev/test/infra/http/http_adapter_test.dart
+++ fordev/test/infra/http/http_adapter_test.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fordev/infra/http_adapter.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 
@@ -13,6 +11,8 @@ void main() {
   ClientSpy client;
   String url;
   Map<String, String> headers;
+
+  void mockRequest() => when(client.post(any, headers: anyNamed('headers'), body: '{"any_key":"any_value"}')).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
 
   setUp(() {
     client = ClientSpy();
@@ -25,46 +25,43 @@ void main() {
     'Post Requests',
     () {
       test('Should call Post with correct Url', () async {
-        await sut.request(
-          url: url,
-          method: 'post',
-        );
+        // when(client.post(any, headers: anyNamed('headers'), body: '{"any_key":"any_value"}')).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+        mockRequest();
 
-        verify(client.post(Uri.parse(url), headers: headers));
+        await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
+
+        verify(client.post(Uri.parse(url), headers: headers, body: '{"any_key":"any_value"}'));
       });
 
       test('Should call Post with correct Headers values', () async {
-        await sut.request(url: url, method: 'post');
+        mockRequest();
+        await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
-        verify(client.post(Uri.parse(url), headers: headers));
+        verify(client.post(Uri.parse(url), headers: headers, body: '{"any_key":"any_value"}'));
       });
 
       test('Should call Post with correct Body', () async {
+        mockRequest();
         await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
         verify(client.post(Uri.parse(url), headers: headers, body: '{"any_key":"any_value"}'));
       });
 
       test('Should call Post without Body', () async {
+        when(client.post(any, headers: anyNamed('headers'))).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+
         await sut.request(url: url, method: 'post');
 
-        verify(client.post(
-          any,
-          headers: anyNamed('headers'),
-        ));
+        verify(client.post(any, headers: anyNamed('headers')));
+      });
+
+      test('Should return data if post returns 200', () async {
+        when(client.post(any, headers: anyNamed('headers'))).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+
+        final response = await sut.request(url: url, method: 'post');
+
+        expect(response, {"any_key": "any_value"});
       });
     },
   );
-}
-
-class HttpAdapter {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<void> request({@required String url, @required String method, Map body}) async {
-    final headers = {'content-type': 'application/json', 'accept': 'application/json'};
-
-    await client.post(Uri.parse(url), headers: headers, body: body != null ? jsonEncode(body) : null);
-  }
 }
