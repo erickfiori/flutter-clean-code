7d3d1dc3cb183572e20905cc131f68f3c233f193
erickfiori <e.felipe.santos@hotmail.com>
2022-03-16 10:48:29 -0300

    Ensure HttpAdapter calls post with correct Headers Values

--- fordev/test/infra/http/http_adapter_test.dart
+++ fordev/test/infra/http/http_adapter_test.dart
@@ -7,17 +7,34 @@ import 'package:mockito/mockito.dart';
 class ClientSpy extends Mock implements Client {}
 
 void main() {
-  group(
-    'Post',
-    () {
-      test('Should call Post with correct values', () async {
-        final client = ClientSpy();
-        final sut = HttpAdapter(client);
-        final url = faker.internet.httpUrl();
+  HttpAdapter sut;
+  ClientSpy client;
+  String url;
+  Map<String, String> headers;
 
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+
+    headers = {'content-type': 'application/json', 'accept': 'application/json'};
+  });
+  group(
+    'Post Requests',
+    () {
+      test('Should call Post with correct Url', () async {
+        await sut.request(
+          url: url,
+          method: 'post',
+        );
+
+        verify(client.post(Uri.parse(url), headers: headers));
+      });
+
+      test('Should call Post with correct Headers values', () async {
         await sut.request(url: url, method: 'post');
 
-        verify(client.post(Uri.parse(url)));
+        verify(client.post(Uri.parse(url), headers: headers));
       });
     },
   );
@@ -29,6 +46,8 @@ class HttpAdapter {
   HttpAdapter(this.client);
 
   Future<void> request({@required String url, @required String method, Map body}) async {
-    await client.post(Uri.parse(url));
+    final headers = {'content-type': 'application/json', 'accept': 'application/json'};
+
+    await client.post(Uri.parse(url), headers: headers);
   }
 }
